eef6db1382e330eeab5f87bbb8fdf8b0ad2d3943
N3STOR173 <nestorgmayor@gmail.com>
2026-02-13 14:52:29 +0100

    evaluacion cromosoma binario

--- src/main/java/org/pr1/cromosomas/CromosomaBinario.java
+++ src/main/java/org/pr1/cromosomas/CromosomaBinario.java
@@ -1,19 +1,51 @@
 package org.pr1.cromosomas;
 
+import org.pr1.Scene;
+import org.pr1.evaluacion.EvaluacionBinaria;
+
 import java.lang.reflect.Array;
+import java.util.ArrayList;
 
-public class CromosomaBinario {
+public class CromosomaBinario implements Cromosoma {
+    /*
+    Presion selectiva en las graficas
+    Puede haber fitnes negativos esos datos no se cogen con la ruleta
+    es decir se ponen a 0 por como funciona la ruleta
+    Las posiciones fuera del tablero se arreglan y las columnas se penalizan
+     */
+    private boolean[][] cromosoma;
+    private Scene scene;
+    private int numCamaras;
+    private int evaluacion;
 
-    boolean[][] cromosoma;
+    CromosomaBinario(int numCamaras, Scene scene) {
+        //se coloca la escena y las camaras
+        this.scene = scene;
+        this.numCamaras = numCamaras;
 
-    CromosomaBinario(int numCamaras, int x, int y) {
+        //se crea el array del cromosoma
+        boolean[][] matriz = new boolean[numCamaras * 2][];
+
         //saca el numero de bits necesarios para la longitud x
-        double logBase2 = Math.log(x) / Math.log(2);
-        int bitsX = (int) Math.ceil(logBase2);
+        double logBase2X = Math.log(scene.getCols()) / Math.log(2);
+        int bitsX = (int) Math.ceil(logBase2X);
 
         //saca el numero de bits necesarios para la longitud y
+        double logBase2Y = Math.log(scene.getRows()) / Math.log(2);
+        int bitsY = (int) Math.ceil(logBase2Y);
 
+        //se itera creando los genes con sus respectivas cantidades de bits
+        for (int i = 0; i <= numCamaras; i++) {
+            this.cromosoma[i * 2] = new boolean[bitsX];
+            this.cromosoma[i * 2 + 1] = new boolean[bitsY];
+        }
 
-
+        this.evaluacion = EvaluacionBinaria.evaluar(this);
     }
+
+    public int evaluar() {return evaluacion;}
+
+    public boolean[][] getCromosoma() {return cromosoma;}
+    public Scene getScene() {return scene;}
+    public int getNumCamaras() {return numCamaras;}
 }
---
+++ src/main/java/org/pr1/evaluacion/EvaluacionBinaria.java
@@ -0,0 +1,115 @@
+package org.pr1.evaluacion;
+
+import org.pr1.Scene;
+import org.pr1.cromosomas.CromosomaBinario;
+
+public class EvaluacionBinaria{
+    //DATOS
+    static boolean[][] visited;
+
+    //Devuelve el numero de celdas visibles
+    public static int evaluar(CromosomaBinario cromosoma) {
+        //escena a la que se refiere el cromosoma
+        Scene scene = cromosoma.getScene();
+
+        //estructura de datos para no contar repetidas las baldosas
+        visited = new boolean[scene.getRows()][scene.getCols()];
+
+        //estructura de datos para guardar el total de baldosas
+        int total = 0;
+
+        //se itera por cada camara
+        int numeroCamaras = cromosoma.getNumCamaras();
+        boolean[][] datos = cromosoma.getCromosoma();
+        for (int i = 0; i < numeroCamaras; i++) {
+            //parsea la posicion en binario a entero
+            int posX = parsearBinario(datos[i * 2]);
+            int posY = parsearBinario(datos[i * 2 + 1]);
+
+            //posicion de la camara
+            int[] pos = new int[]{posX, posY};
+
+            //se mira si la camara esta en una columna
+            int baldosa = scene.getGrid()[posY][posX];
+
+            //si no es ponderado y hay un 1 significa columna
+            if (!scene.isPonderado() && baldosa == 1) {
+                total -= 100;
+                continue;
+            }
+            //si es ponderado y ha un 0 significa columna
+            else if (scene.isPonderado() && baldosa == 0) {
+                total -= 100;
+                continue;
+            }
+
+            //se suman las puntuaciones si no esta en una columna
+            total += avanzar(scene, pos, 0, -1);
+            total += avanzar(scene, pos, 0, 1);
+            total += avanzar(scene, pos, 1, -1);
+            total += avanzar(scene, pos, 1, 1);
+        }
+
+        return total;
+    }
+
+    private static int parsearBinario(boolean[] bits) {
+        int resultado = 0;
+
+        for (int i = 0; i < bits.length; i++) {
+            //se desplaza a la izquierda el resultado actual
+            resultado <<= 1;
+            if (bits[i]) {
+                //se pone el bit actual a 1
+                resultado |= 1;
+            }
+        }
+
+        return resultado;
+    }
+
+    /*
+    Recibe:
+    ini coordenada desde la que se empieza X o Y dependiendo de cual sea el eje
+    eje con 0 para X y 1 para Y
+    avance con -1 para hacia atras y 1 hacia delante
+     */
+    private static int avanzar(Scene scene, int[] baldosa, int eje, int avance) {
+        //limte al avanzar hacia delante
+        int limiteHaciaDelante = 0;
+
+        //valor dependiendo del eje
+        if (eje == 0)
+            limiteHaciaDelante = scene.getCols();
+        else if (eje == 1)
+            limiteHaciaDelante = scene.getRows();
+
+        //baldosas iluminadas que no estaban ya iluminadas
+        int puntuacion = 0;
+
+        //si se ha salido de los limites para
+        while (baldosa[eje] > 0 && baldosa[eje] < limiteHaciaDelante) {
+            //si no es ponderado y hay un 1 es una columna
+            if (!scene.isPonderado() && scene.getGrid()[baldosa[1]][baldosa[0]] == 1)
+                break;
+            //si es ponderado y hay un 0 es una columna
+            if (scene.isPonderado() && scene.getGrid()[baldosa[1]][baldosa[0]] == 0)
+                break;
+
+            //si no se ha visitado ya la balsdosa se suma la puntuacion
+            if (!visited[baldosa[1]][baldosa[0]]) {
+                //si no es ponderado se suma 1
+                if (!scene.isPonderado())
+                    puntuacion++;
+                else
+                    puntuacion += scene.getGrid()[baldosa[0]][baldosa[1]];
+            }
+            visited[baldosa[1]][baldosa[0]] = true;
+
+            //se coge la siguiente baldosa
+            baldosa[eje] += avance;
+        }
+
+        return puntuacion;
+    }
+}

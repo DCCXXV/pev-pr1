81329cbec28e1c90022b43408a3d3b1ab621b1e2
DCCXXV <110636189+DCCXXV@users.noreply.github.com>
2026-02-18 14:59:35 +0100

    feat(Cruce): cruce monopunto bin/real

---
+++ src/main/java/org/pr1/ag/cruce/CruceMonopuntoBin.java
@@ -0,0 +1,28 @@
+package org.pr1.ag.cruce;
+
+import java.util.Random;
+import org.pr1.cromosomas.Cromosoma;
+import org.pr1.cromosomas.CromosomaBinario;
+
+public class CruceMonopuntoBin implements Cruce {
+
+    private Random rng = new Random();
+
+    @Override
+    public void cruzar(Cromosoma padre1, Cromosoma padre2) {
+        CromosomaBinario p1 = (CromosomaBinario) padre1;
+        CromosomaBinario p2 = (CromosomaBinario) padre2;
+
+        boolean[][] genes1 = p1.getGenes();
+        boolean[][] genes2 = p2.getGenes();
+
+        // punto de corte elegido al azar
+        int puntoCorte = rng.nextInt(genes1.length);
+
+        for (int i = puntoCorte; i < genes1.length; i++) {
+            boolean[] temp = genes1[i];
+            genes1[i] = genes2[i];
+            genes2[i] = temp;
+        }
+    }
+}
--- src/main/java/org/pr1/ag/cruce/CruceMonopuntoReal.java
+++ src/main/java/org/pr1/ag/cruce/CruceMonopuntoReal.java
@@ -2,25 +2,25 @@ package org.pr1.ag.cruce;
 
 import java.util.Random;
 import org.pr1.cromosomas.Cromosoma;
-import org.pr1.cromosomas.CromosomaBinario;
+import org.pr1.cromosomas.CromosomaReal;
 
-public class CruceMonopunto implements Cruce {
+public class CruceMonopuntoReal implements Cruce {
 
     private Random rng = new Random();
 
     @Override
     public void cruzar(Cromosoma padre1, Cromosoma padre2) {
-        CromosomaBinario p1 = (CromosomaBinario) padre1;
-        CromosomaBinario p2 = (CromosomaBinario) padre2;
+        CromosomaReal p1 = (CromosomaReal) padre1;
+        CromosomaReal p2 = (CromosomaReal) padre2;
 
-        boolean[][] genes1 = p1.getGenes();
-        boolean[][] genes2 = p2.getGenes();
+        double[] genes1 = p1.getGenes();
+        double[] genes2 = p2.getGenes();
 
         // punto de corte elegido al azar
         int puntoCorte = rng.nextInt(genes1.length);
 
         for (int i = puntoCorte; i < genes1.length; i++) {
-            boolean[] temp = genes1[i];
+            double temp = genes1[i];
             genes1[i] = genes2[i];
             genes2[i] = temp;
         }
